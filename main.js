67fdd96036077f2f217eab6e44ca73f38b281885
Haitam Skhairi <haitam.skhairi@gmail.com>
2022-06-07 21:33:06 +0100

    Finish the styling of the About Page

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// Button Scroll To Top
+let button = document.querySelectorAll(".scrolling-button");
+
+window.onscroll = () => {
+  if (windows.scrollY >= 300) {
+    button.forEach((element) => {
+      element.style.display = "block";
+    });
+  } else {
+    button.forEach((element) => {
+      element.style.display = "none";
+    });
+  }
+};
+
 // Nav bar
 
 let navBarLinks = document.querySelectorAll(".header .links li a");
@@ -8,8 +23,22 @@ const toggelMenu = document.querySelector(".header .toggle-menu");
 const toggelIcon = document.querySelector(".header .toggle-icon");
 
 toggelIcon.onclick = function () {
-  toggelMenu.classList.toggle("show");
+  toggelMenu.classList.toggle("open");
+  toggelIcon.classList.toggle("active");
 };
+
+// Stop Propagation
+toggelMenu.onclick = function (e) {
+  e.stopPropagation();
+};
+
+// Close Toggle Bav If Clicking Outside it
+
+document.addEventListener("click", (e) => {
+  if (e.target !== toggelMenu && e.target !== toggelIcon) {
+    toggelMenu.classList.remove("open");
+  }
+});
 
 // Put Active Class Function
 
@@ -23,3 +52,13 @@ function putActiveClass(array) {
     });
   });
 }
+
+// Skills Progress
+
+let skills = document.querySelectorAll(".about-me .skills span");
+
+setTimeout(() => {
+  skills.forEach((span) => {
+    span.style.width = span.dataset.width;
+  });
+}, 5000);
